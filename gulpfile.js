82901cce367a02fde85f0179295640b7e6eefcb3
Devin Otway <devin.otway@gmail.com>
2016-05-14 18:33:08 -0700

    add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,41 @@
+'use strict'
+
 const gulp = require('gulp')
-const sass = require('sass')
+const sass = require('gulp-sass')
+const del = require('del')
 
 let paths = {
 	styles:  {
-		entry: './styles/**/*.scss'
+		entry: './styles/**/*.scss',
+		out: './public/styles/'
+	},
+	html: {
+		entry: './src/index.html',
+		out: './public/'
 	}
 }
 
 gulp.task('sass', ()=>{
 	return gulp.src(paths.styles.entry)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('.css'))
+		.pipe(gulp.dest(paths.styles.out))
 })
 
 gulp.task('sass:watch', ()=>{
 	gulp.watch(paths.styles.entry)
 })
+
+gulp.task('copy', ()=>{
+	gulp.src(paths.html.entry).pipe(gulp.dest(paths.html.out))
+})
+
+gulp.task('clean', ()=> {
+	return del([
+			'./public/styles/*'
+		])
+})
+
+gulp.task('default', ['clean', 'copy'], ()=>{
+	gulp.start('sass', 'sass:watch')
+})
+
